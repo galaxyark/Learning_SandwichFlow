bb0cd4597e5ebf1bd47fccefcbc99a7f152c234a
GalaXyArk <lfz.ark@gmail.com>
2013-10-10 18:24:08 -0700

    Initial

---
+++ SandwichFlow/DynamicSandwichViewController.h
@@ -0,0 +1,13 @@
+//
+//  DynamicSandwichViewController.h
+//  SandwichFlow
+//
+//  Created by Fangzhou Lu on 9/28/13.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicSandwichViewController : UIViewController
+
+@end
---
+++ SandwichFlow/DynamicSandwichViewController.m
@@ -0,0 +1,130 @@
+//
+//  DynamicSandwichViewController.m
+//  SandwichFlow
+//
+//  Created by Fangzhou Lu on 9/28/13.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import "DynamicSandwichViewController.h"
+#import "SandwichViewController.h"
+#import "AppDelegate.h"
+
+@interface DynamicSandwichViewController ()
+
+@end
+
+@implementation DynamicSandwichViewController{
+    NSMutableArray *_views;
+    UIGravityBehavior *_gravity;
+    UIDynamicAnimator *_animator;
+    CGPoint _previousTouchPoint;
+    BOOL _draggingView;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Background image
+    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Background-LowerLayer.png"]];
+    [self.view addSubview:backgroundImageView];
+    
+    //header logo
+    UIImageView* header = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Sarnie.png"]];
+    header.center = CGPointMake(220, 190);
+    [self.view addSubview:header];
+    
+    _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    _gravity = [[UIGravityBehavior alloc] init];
+    [_animator addBehavior:_gravity];
+    _gravity.magnitude = 4.0f;
+    
+    _views = [NSMutableArray new];
+    float offset = 250.0f;
+    for (NSDictionary *sandwich in [self sandwiches]) {
+        [_views addObject:[self addRecipeAtOffset:offset forSandwich:sandwich]];
+        offset -= 50.0f;
+    }
+}
+
+- (NSArray *)sandwiches
+{
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    return  appDelegate.sandwiches;
+}
+
+- (UIView *)addRecipeAtOffset:(float)offset forSandwich:(NSDictionary *)sandwich
+{
+    CGRect frameForView = CGRectOffset(self.view.bounds, 0.0, self.view.bounds.size.height - offset);
+    
+    // 1. Create the view controller
+    UIStoryboard *mystroryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    SandwichViewController *viewController = [mystroryboard instantiateViewControllerWithIdentifier:@"SandwichVC"];
+    
+    // 2. Set the frame and provide some data
+    UIView *view = viewController.view;
+    view.frame = frameForView;
+    viewController.sandwich = sandwich;
+    
+    // 3. add as a child
+    [self addChildViewController:viewController];
+    [self.view addSubview:view];
+    [viewController didMoveToParentViewController:self];
+    
+    // 4. Add a gesture recognizer
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [viewController.view addGestureRecognizer:pan];
+    
+    // 5. Create a collision
+    UICollisionBehavior *collision = [[UICollisionBehavior alloc] initWithItems:@[view]];
+    [_animator addBehavior:collision];
+    
+    // 6. lower boundary, where the tab rests.
+    float boundary = view.frame.origin.y + view.frame.size.height + 1;
+    CGPoint boundaryStart = CGPointMake(0.0, boundary);
+    CGPoint boundaryEnd = CGPointMake(self.view.bounds.size.width, boundary);
+    [collision addBoundaryWithIdentifier:@1 fromPoint:boundaryStart toPoint:boundaryEnd];
+    
+    // 7. apply some gravity
+    [_gravity addItem:view];
+    
+    return view;
+}
+
+- (void)handlePan:(UIPanGestureRecognizer *)gesture
+{
+    CGPoint touchPoint = [gesture locationInView:self.view];
+    UIView *draggedView = gesture.view;
+    
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        CGPoint dragStartLocation = [gesture locationInView:draggedView];
+        if (dragStartLocation.y < 200.0f) {
+            _draggingView = YES;
+            _previousTouchPoint = touchPoint;
+        }
+    }else if (gesture.state == UIGestureRecognizerStateChanged && _draggingView) {
+        float yOffset = _previousTouchPoint.y - touchPoint.y;
+        gesture.view.center = CGPointMake(draggedView.center.x, draggedView.center.y - yOffset);
+        _previousTouchPoint = touchPoint;
+    } else if (gesture.state == UIGestureRecognizerStateEnded && _draggingView) {
+        [_animator updateItemUsingCurrentState:draggedView];
+        _draggingView = NO;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
